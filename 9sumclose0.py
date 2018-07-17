a5fddd7c900fe31af22be448c91fb19e17c6bf2b
JoelGodfrey15 <40669479+JoelGodfrey15@users.noreply.github.com>
2018-07-17 18:00:44 +0530

    Update 9sumclose0.py

--- 9sumclose0.py
+++ 9sumclose0.py
@@ -1,12 +1,13 @@
 def main():
     n=int(input())
-    l=[]
+    q=input()
     a=10000
+    l=q.split()
     for i in range(n):
-        l.append(int(input()))
+        l[i]=int(l[i])
     for i in range(len(l)):
         for j in range(i+1,len(l)):
-            if (l[i]+l[j])<a:
+            if abs(l[i]+l[j])<a:
                 a=l[i]+l[j]
                 b,c=l[i],l[j]
     print(b,' ',c)
