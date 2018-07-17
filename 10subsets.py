0783bf50282746ecdfc7894b8398985f0911a10d
JoelGodfrey15 <40669479+JoelGodfrey15@users.noreply.github.com>
2018-07-17 18:15:32 +0530

    Update 10subsets.py

--- 10subsets.py
+++ 10subsets.py
@@ -1,18 +1,21 @@
 def main():
-    n=int(input())
-    m=int(input())
-    a=[]
-    b=[]
+    p=input()
+    l=p.split()
+    n=int(l[0])
+    m=int(l[1])
+    c=input()
+    a=c.split()
+    v=input()
+    b=v.split()
     for i in range(n):
-        a.append(int(input()))
+        a[i]=int(a[i])
     for i in range(m):
-        b.append(int(input()))
+        b[i]=int(b[i])
     a=set(a)
     b=set(b)
     if b<=a:
         print('YES')
     else:
         print('NO')
-
 if __name__ == '__main__':
     main()
