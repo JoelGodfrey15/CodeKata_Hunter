0f9fe43e18cf1e0232361d32b9814c2bc5a531de
JoelGodfrey15 <40669479+JoelGodfrey15@users.noreply.github.com>
2018-07-17 17:30:24 +0530

    Update 7evenodd.py

--- 7evenodd.py
+++ 7evenodd.py
@@ -2,11 +2,14 @@ def main():
     n=int(input())
     a=input()
     l=a.split()
+    k=[]
     for i in range(n):
         l[i]=int(l[i])
     for i in range(len(l)):
         if(l[i]%2==0 and i%2==1) or (l[i]%2==1 and i%2==0):
-            print(l[i],end=' ')
+            k.append(str(l[i]))
+    b=' '.join(k)
+    print(b)
 
 if __name__ == '__main__':
     main()
